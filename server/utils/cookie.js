4985bfedf217ce77ccd6be2cff935236415348b8
Jaivardhan7773 <jaivardhansinghrathore17@gmail.com>
2025-08-31 00:37:40 +0530

    Completed backend setup of refreshed tokens

--- server/utils/cookie.js
+++ server/utils/cookie.js
@@ -1,13 +1,13 @@
 import jwt from "jsonwebtoken";
 
 export const generateAccessToken = (userId) => {
-    return jwt.sign({ userId }, process.env.JWT_SECRET, {
+    return jwt.sign({ userId }, process.env.JWT_ACCESS_SECRET, {
         expiresIn: "15m"
     });
 }
 
 export const generateRefreshToken = (userId) => {
-    return jwt.sign({ userId }, process.env.JWT_SECRET, {
+    return jwt.sign({ userId }, process.env.JWT_REFRESH_SECRET, {
         expiresIn: "7d"
     });
 }
