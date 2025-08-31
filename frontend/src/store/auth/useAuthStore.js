268ea84cbc3a70df3e974f3a1e55c5bbbbc2a6a3
Jaivardhan7773 <jaivardhansinghrathore17@gmail.com>
2025-08-31 23:23:27 +0530

    Frontend login setup

--- frontend/src/store/auth/useAuthStore.js
+++ frontend/src/store/auth/useAuthStore.js
@@ -1,14 +1,63 @@
 import { create } from "zustand"
 import { axiosInstance } from "../../utils/axios"
+import { toast } from "react-toastify";
 
 
-export const useAuthStore = create((set , get) => ({
-    authUser : null,
-    isSigningUp : false,
-    isLoginingUp:false,
-    isCheckingAuth:true,
+export const useAuthStore = create((set, get) => ({
+    user: null,
+    loading: false,
+    error: null,
 
-    
+    login: async (email, password) => {
+        set({ loading: true });
+        try {
+            const res = await axiosInstance.post('/login', { email, password });
+            set({ user: res.data, error: null });
+            toast.success("login successfull")
+        } catch (err) {
+            const backendData = err.response?.data;
+
+            // Check for express-validator errors
+            let errorMessage = "";
+            if (backendData?.error) {
+                // Take the first validation error message
+                errorMessage = backendData.error[0].msg;
+            } else if (backendData?.message) {
+                errorMessage = backendData.message;
+            } else {
+                errorMessage = "Login failed";
+            }
+
+            set({ error: errorMessage, loading: false });
+            toast.error(errorMessage);
+        } finally {
+            set({ loading: false })
+        }
+    },
+
+    logout: async () => {
+        set({ loading: true });
+        try {
+            await axiosInstance.post('/logout')
+            set({ user: null })
+            toast.success("logout successfull");
+        } catch (error) {
+            toast.error("Error in logout")
+        } finally {
+            set({ loading: false })
+        }
+    },
+
+    checkAuth: async () => {
+        try {
+            const res = await axiosInstance.get("/auth/me");
+            set({ user: res.data });
+        } catch (err) {
+            set({ user: null });
+        }
+    },
+
+
 
 
 }))
--- server/server.js
+++ server/server.js
@@ -9,16 +9,13 @@ dotenv.config();
 connectDB();
 
 const app = express();
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true
+}))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-
-
-// app.use(cookieParser());
-// app.use(cors({
-//     origin:"http://localhost:5173",
-//     credentials:true
-// }))
 
 app.use('/api', userRoute)
 
