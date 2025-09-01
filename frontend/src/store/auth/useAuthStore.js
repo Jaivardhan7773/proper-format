d8db91dfdc4294f069b609130b3d7e1053f4c134
Jaivardhan7773 <jaivardhansinghrathore17@gmail.com>
2025-09-01 21:23:01 +0530

    add access token auto-refresh with axios interceptor

--- frontend/src/store/auth/useAuthStore.js
+++ frontend/src/store/auth/useAuthStore.js
@@ -7,17 +7,30 @@ export const useAuthStore = create((set, get) => ({
     user: null,
     loading: false,
     error: null,
-    isCheckingAuth: false,
+    isCheckingAuth: true,
+
+    checkAuth: async () => {
+        try {
+            const res = await axiosInstance.get("/check");
+            set({ user: res.data });
+            return true;
+        } catch (err) {
+            console.error("Error checking authentication:", err);
+            set({ user: null });
+        } finally {
+            set({ isCheckingAuth: false });
+        }
+    },
 
     login: async (email, password) => {
         set({ loading: true });
         try {
             const res = await axiosInstance.post('/login', { email, password });
             set({ user: res.data, error: null });
-            toast.success("login successfull")
+            toast.success("login successfull");
+            return true;
         } catch (err) {
             const backendData = err.response?.data;
-
             // Check for express-validator errors
             let errorMessage = "";
             if (backendData?.error) {
@@ -31,35 +44,57 @@ export const useAuthStore = create((set, get) => ({
 
             set({ error: errorMessage, loading: false });
             toast.error(errorMessage);
+            return false;
         } finally {
             set({ loading: false })
         }
+    },
+
+    Signup: async (name, email, password) => {
+        set({ loading: true });
+        try {
+            const res = await axiosInstance.post('/signup', { name, email, password });
+            set({ user: res.data, error: null });
+            toast.success("signup success")
+            return true;
+        } catch (err) {
+            const backendData = err.response?.data;
+            // Check for express-validator errors
+            let errorMessage = "";
+            if (backendData?.error) {
+                // Take the first validation error message
+                errorMessage = backendData.error[0].msg;
+            } else if (backendData?.message) {
+                errorMessage = backendData.message;
+            } else {
+                errorMessage = "Signup failed";
+            }
+
+            set({ error: errorMessage, loading: false });
+            toast.error(errorMessage);
+            return false;
+        } finally {
+            set({ loading: false });
+        }
+
     },
 
     logout: async () => {
-        set({ loading: true });
         try {
-            await axiosInstance.post('/logout')
-            set({ user: null })
-            toast.success("logout successfull");
+            const { user } = get();
+            if (user) {
+
+                await axiosInstance.post('/logout')
+                set({ user: null })
+                toast.success("logout successfull");
+            }
         } catch (error) {
-            toast.error("Error in logout")
-        } finally {
-            set({ loading: false })
+            const message = error.response?.data?.message || "Failed to logout";
+            toast.error(message);
         }
     },
 
-    checkAuth: async () => {
-        set({ isCheckingAuth: true, error: null });
-        try {
-            const res = await axiosInstance.get("/check");
-            set({ user: res.data, isCheckingAuth: false });
-            return true;
-        } catch (err) {
-            set({ user: null, isCheckingAuth: false });
-            return false;
-        }
-    }
+
 
 
 
