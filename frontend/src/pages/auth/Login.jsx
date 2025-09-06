b49539e0f90b7b70d6869fc2f4ec33036ccf111b
Jaivardhan7773 <jaivardhansinghrathore17@gmail.com>
2025-09-06 23:11:29 +0530

    Best practices

--- frontend/src/pages/auth/Login.jsx
+++ frontend/src/pages/auth/Login.jsx
@@ -19,7 +19,7 @@ const Login = () => {
   return (
     <>
 
-      <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8 ">
+      <div className="flex h-screen flex-col justify-center px-6 py-12 lg:px-8 ">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           <img src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company" className="mx-auto h-10 w-auto" />
           <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-white">Sign in to your account</h2>
