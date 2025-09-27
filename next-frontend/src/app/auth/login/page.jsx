5e1d6a25f67d707b9251a55e82298679d0a4026a
Jaivardhan7773 <jaivardhansinghrathore17@gmail.com>
2025-09-27 23:25:55 +0530

    ?

--- next-frontend/src/app/auth/login/page.jsx
+++ next-frontend/src/app/auth/login/page.jsx
@@ -1,9 +1,83 @@
-import React from 'react'
+"use client"
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
+import { useAuthStore } from "../../../store/auth/useAuthStore";
+import GuestGuard from "@/components/GuestGuard";
 
-const page = () => {
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { login, error,  loading } = useAuthStore();
+  const router = useRouter();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const result =  await login(email, password);
+   if (result?.success) {
+            router.push("/");
+        } else {
+            console.log("Login failed");
+        }
+  }
+
   return (
-    <div>page</div>
+    <GuestGuard>
+    <>
+
+      <div className="flex h-screen flex-col justify-center px-6 py-12 lg:px-8 ">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <img src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company" className="mx-auto h-10 w-auto" />
+          <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-white">Sign in to your account</h2>
+        </div>
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form onSubmit={handleSubmit} method="POST" className="space-y-6">
+            <div>
+              <label htmlFor="email" className="block text-sm/6 font-medium text-gray-100">Email address</label>
+              <div className="mt-2">
+                <input id="email" type="email" name="email" required
+                  autoComplete="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" />
+              </div>
+            </div>
+
+            <div>
+              <div className="flex items-center justify-between">
+                <label htmlFor="password" className="block text-sm/6 font-medium text-gray-100">Password</label>
+                <div className="text-sm">
+                  <a href="#" className="font-semibold text-indigo-400 hover:text-indigo-300">Forgot password?</a>
+                </div>
+              </div>
+              <div className="mt-2">
+                <input id="password" type="password" name="password" required autoComplete="current-password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" />
+              </div>
+            </div>
+
+            <div>
+              <button disabled={loading} type="submit" className="flex w-full justify-center rounded-md bg-indigo-500 px-3 py-1.5 text-sm/6 font-semibold text-white hover:bg-indigo-800 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500 cursor-pointer">
+                {loading ? "Logging in..." : "Login"}
+              </button>
+              {error && <p style={{ color: "red" }}>{error}</p>}
+            </div>
+          </form>
+
+          <p className="mt-10 text-center text-sm/6 text-gray-400">
+            Not a member?
+            <Link href={"/auth/signup"} className="font-semibold text-indigo-400 hover:text-indigo-300">Start a 14 day free trial</Link>
+          </p>
+        </div>
+      </div>
+
+    </>
+    </GuestGuard>
   )
 }
 
-export default page
+export default Login;
